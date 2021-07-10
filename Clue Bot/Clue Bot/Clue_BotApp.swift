da9bfd488be5c6080dd1bbd6c64e18549fb95b54
Daniel Sedlov <mr.sedlov@gmail.com>
2021-07-10 13:05:54 -0300

    Initial Commit

---
+++ Clue Bot/Clue Bot/Clue_BotApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clue_BotApp.swift
+//  Clue Bot
+//
+//  Created by Danny Sedlov on 2021-07-10.
+//
+
+import SwiftUI
+
+@main
+struct Clue_BotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
